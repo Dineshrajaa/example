c22bb5ec117be2e06ee581167c461a7919bbc5f0
Dinesh Raja <dineshrajaa.93@gmail.com>
2018-07-28 12:47:21 +0530

    Code change to demonstrate missing translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {TranslateService} from '@ngx-translate/core';
   template: `
     <div>
       <h2>{{ 'HOME.TITLE' | translate }}</h2>
+      <h3>{{'HOME.DEFAULT' | translate}}</h3>
       <label>
         {{ 'HOME.SELECT' | translate }}
         <select #langSelect (change)="translate.use(langSelect.value)">
@@ -17,10 +18,10 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class AppComponent {
   constructor(public translate: TranslateService) {
-    translate.addLangs(['en', 'fr']);
-    translate.setDefaultLang('en');
+    translate.addLangs(['en-uk', 'en-us']);
+    translate.setDefaultLang('en-uk');
 
     const browserLang = translate.getBrowserLang();
-    translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+    translate.use(browserLang.match(/en|fr/) ? 'en-us' : 'en-uk');
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, PipeTransform, Inject } from '@angular/core';
 import { AppComponent } from './app.component';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateModule, TranslateLoader, MissingTranslationHandler, TranslatePipe, MissingTranslationHandlerParams } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient);
 }
 
+class CustomHandler implements MissingTranslationHandler {
+  handle(params: MissingTranslationHandlerParams) {
+    if (params.translateService.currentLang === params.translateService.defaultLang) {
+      return params.key;
+    } else {
+      return '[MISSING]: ' + params.key;
+    }
+  }
+}
 @NgModule({
   declarations: [
     AppComponent
@@ -22,10 +31,12 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
         deps: [HttpClient]
-      }
+      },
+      missingTranslationHandler: { provide: MissingTranslationHandler, useClass: CustomHandler }
     })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
